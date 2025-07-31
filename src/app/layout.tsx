deaf0a0dfdca136d4fa2d396feff3fd4cbca6b0a
mark <mark.mekhayl@gmail.com>
2025-07-31 18:59:33 +1000

    added speed insights on main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights} from "@vercel/speed-insights/next";
 import Script from 'next/script';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -58,6 +59,7 @@ export default function RootLayout({
 
         <Footer />
       <Analytics />
+      <SpeedInsights />
       </body>
     </html>
   );
