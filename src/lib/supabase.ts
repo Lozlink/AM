a279153bedffb4dd03e805e8b42d19a3ca7856b9
Lozlink <mark.mekhayl@gmail.com>
2026-01-27 16:02:16 +1100

    sb: updated supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const supabaseService = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY!
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!
+const supabaseService = process.env.SUPABASE_SERVICE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   realtime: {
